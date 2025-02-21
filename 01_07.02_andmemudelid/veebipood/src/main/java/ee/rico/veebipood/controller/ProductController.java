3f5f818e6af233d19fc705d6b59b696d7c4b77a5
Croccu <144142847+Croccu@users.noreply.github.com>
2025-02-21 11:52:45 +0200

    vigadeparandus

--- 01_07.02_andmemudelid/veebipood/src/main/java/ee/rico/veebipood/controller/ProductController.java
+++ 01_07.02_andmemudelid/veebipood/src/main/java/ee/rico/veebipood/controller/ProductController.java
@@ -1,14 +1,30 @@
 package ee.rico.veebipood.controller;
 
+import ee.rico.veebipood.model.Product;
+import ee.rico.veebipood.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
 
+    @Autowired
+    ProductRepository productRepository;
+
     // localhost:8080/products
     @GetMapping("products")
-    public String getProducts() {
-        return "Hello world";
+    public List<Product> getProducts() {
+        return productRepository.findAll(); // SELECT * FROM extends JpaRepository<Product>
+    }
+
+    @PostMapping("products")
+    public List<Product> addProduct(@RequestBody Product product) {
+        productRepository.save(product); // INSERT INTO products
+        return productRepository.findAll();
     }
 }
--- 01_07.02_andmemudelid/veebipood/src/main/java/ee/rico/veebipood/model/Product.java
+++ 01_07.02_andmemudelid/veebipood/src/main/java/ee/rico/veebipood/model/Product.java
@@ -9,16 +9,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 
 public class Product {
-    @Id;
-    @GeneratedValue(strategy = GenerationType.IDENTITY);
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private double price;
