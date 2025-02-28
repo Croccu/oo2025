e32576e62b3db7f603814435a2404598e5dc9c14
Croccu <144142847+Croccu@users.noreply.github.com>
2025-02-28 13:15:21 +0200

    Sliding_average logic implemented

--- 04_28.02_proovikontrolltoo1/slipping_average/src/main/java/ee/rico/slipping_average/Controller/ValueController.java
+++ 04_28.02_proovikontrolltoo1/slipping_average/src/main/java/ee/rico/slipping_average/Controller/ValueController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -24,6 +25,39 @@ public class ValueController {
     public List<ValueEntity> addValue(@RequestBody ValueEntity valueEntity) {
         valueRepository.save(valueEntity);
         return valueRepository.findAll();
+    }
+
+    @GetMapping("values/sum")
+    public int sum() {
+        return valueRepository.findAll().stream().mapToInt(ValueEntity::getValue).sum();
+    }
+
+    @GetMapping("values/average")
+    public double average() {
+        return valueRepository.findAll().stream().mapToInt(ValueEntity::getValue).average().orElse(0);
+    }
+
+    @GetMapping("values/max")
+    public int max() {
+        return valueRepository.findAll().stream().mapToInt(ValueEntity::getValue).max().orElse(0);
+    }
+
+    @GetMapping("values/sliding_average")
+    public List<Double> getSlidingAverage() {
+        List<ValueEntity> valueEntities = valueRepository.findAll();
+        List<Double> slidingAverage = new ArrayList<>();
+
+        if (valueEntities.size() < 3) {
+            return slidingAverage;
+        }
+
+        for (int i = 0; i < valueEntities.size(); i++) {
+            double value = (valueEntities.get(i).getValue() +
+                    valueEntities.get(i + 1).getValue() +
+                    valueEntities.get(i + 1).getValue() / 3.0);
+            slidingAverage.add(value);
+        }
+        return slidingAverage;
     }
 
 }
