387bb9c0037026a15eaaa627f3220677705ab38d
Croccu <144142847+Croccu@users.noreply.github.com>
2025-02-28 12:24:57 +0200

    init slipping_avg

---
+++ 04_28.02_proovikontrolltoo1/slipping_average/src/main/java/ee/rico/slipping_average/SlippingAverageApplication.java
@@ -0,0 +1,13 @@
+package ee.rico.slipping_average;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlippingAverageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlippingAverageApplication.class, args);
+	}
+
+}
---
+++ 04_28.02_proovikontrolltoo1/slipping_average/src/test/java/ee/rico/slipping_average/SlippingAverageApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.rico.slipping_average;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlippingAverageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
